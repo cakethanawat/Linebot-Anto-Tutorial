43153e8bfbb44adf475be3201e6250a7d53b8091
cakethanawat <48070609+cakethanawat@users.noreply.github.com>
2019-03-07 18:00:43 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -10,17 +10,17 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage
 )
 
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+line_bot_api = LineBotApi('/mO2Je84uD9n1o8Vz2qcxDQf8W/hFDykYbDP4q+59dIJ0QDc3+Jr4YUH+LEwzB8wQJtNvUsIFLXF+YX84g9E9rMYbx6R+c7FjP940fRuxYKV7gq0nWpEpbLThjAncEaILZ6eBlJUCQ2JH+/10bmo2wdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('49b0435fe1a54671eebf49671eb0a273')
 
 app = Flask(__name__)
 
 # username of anto.io account
-user = 'YOUR_USERNAME'
+user = 'thanawatboonkear2@gmail.com'
 # key of permission, generated on control panel anto.io
-key = 'YOUR_KEY'
+key = 'QSkbjOxbcOgFp7QJvDkfLxKXvHHv6NwI11HyGswl'
 # your default thing.
-thing = 'YOUR_THING'
+thing = 'myDevice'
 
 anto = antolib.Anto(user, key, thing)
 
@@ -46,6 +46,26 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = event.message.text
+    if(message == 'channel1 on'):
+        anto.pub('myChannel1', 1)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="Turn On channel1"))
+    elif(message == 'channel1 off'):
+        anto.pub('myChannel1', 0)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="Turn Off channel1"))
+    elif(message == 'channel2 on'):
+        anto.pub('myChannel2', 1)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="Turn On channel2"))
+    elif(message == 'channel2 off'):
+        anto.pub('myChannel2', 0)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="Turn Off channel2"))
     # line_bot_api.reply_message(
     #     event.reply_token,
     #     TextSendMessage(text="Turn Off channel1"))
